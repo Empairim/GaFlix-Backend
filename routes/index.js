a1a6de8bb90725e5ca88bf50fb24b74541703096
Empairim <titusfaulknertull@yahoo.com>
2023-11-16 10:51:09 -0500

    added the user routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import horrorRoutes from "./horrorMovie.js";
 import actionRoutes from "./actionMovie.js";
+import userRoutes from "./users"
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.get("/", (req, res) => {
 
 router.use("/horror", horrorRoutes);
 router.use("/action", actionRoutes);
+router.use("/users", userRoutes)
 
 export default router;
