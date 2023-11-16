be5631f2209bb09088be967da2f530efe26c7045
Empairim <titusfaulknertull@yahoo.com>
2023-11-16 11:14:21 -0500

    modified userSchema so that username and email have to be unique

--- models/user.js
+++ models/user.js
@@ -6,9 +6,15 @@ const User = new Schema(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     email: { type: String, required: true },
-    password_digest: { type: String, required: true, select: false },
+    password_digest: {
+      type: String,
+      required: true,
+      select: false,
+      unique: true,
+    },
   },
   { timestamps: true }
 );
