9af0e40db55731e4a04f4e10c42337fcb9344bc6
Empairim <titusfaulknertull@yahoo.com>
2023-11-17 17:06:55 -0500

    update movie controller logic

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -10,3 +10,57 @@ export const getMovies = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const getMovie = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findById(id);
+
+    if (movie) {
+      return res.json(movie);
+    }
+
+    res.status(404).json({ message: "Movie not found" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createMovie = async (req, res) => {
+  try {
+    const movie = new Movie(req.body);
+    await movie.save();
+    res.status(201).json(movie);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateMovie = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findByIdAndUpdate(id, req.body);
+    res.status(201).json(movie);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteMovie = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Movie.findByIdAndDelete(id);
+
+    if (deleted) {
+      return res.status(200).send("Movie Deleted");
+    }
+
+    throw new Error("Movie not found");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
