6371118828d0da44d95518eb8da09e11279b72f8
Empairim <titusfaulknertull@yahoo.com>
2023-11-20 10:57:48 -0500

    updated connection.js to have env url

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
-//so when we update something it returns the new object instead of old one
-mongoose.set("returnOriginal", false);
-//set ups db with local host and after slash is the db name with
-mongoose.connect("mongodb://127.0.0.1:27017/gaflix").catch((err) => {
-  console.log(`Error connection to MongoDB: ${err.message}`);
-});
 
+// Set up mongoose options
+mongoose.set("returnOriginal", false);
+
+// Set up database connection
+mongoose
+  .connect(process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/gaflix")
+  .catch((err) => {
+    console.log(`Error connecting to MongoDB: ${err.message}`);
+  });
+
+// Event listeners for connection status
 mongoose.connection.on("connected", () => {
-  console.log(chalk.bold("connected to MongoDB!"));
+  console.log(chalk.bold("Connected to MongoDB!"));
 });
 
 mongoose.connection.on("disconnected", () => {
